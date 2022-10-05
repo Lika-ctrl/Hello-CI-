1c5f8dd3693a38609610ff20fc5ed69177463b60
Lika Kukharenko <angelikanedoseko@gmail.com>
2022-10-05 20:26:28 +0300

    create helloCI

---
+++ src/main/java/com/lika/ctrl/App.java
@@ -0,0 +1,13 @@
+package com.lika.ctrl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
